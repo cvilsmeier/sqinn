ec42b10b11eaf480fdc4001b8472322519d2968d
moonD4rk <moond4rk13@gmail.com>
2024-01-18 14:27:34 +0800

    ci: Add cross-platform build support with GitHub Actions.
    - Ignore `.github/workflows/build.yml` in the repository
    - Add cross-platform build support with Github Actions workflow
    - Fix typo and add macOS flags in `build.sh` script

--- build.sh
+++ build.sh
@@ -1,28 +1,35 @@
 #!/bin/sh
 
 if [ ! -f "src/main.c" ]; then
-    echo src/main.c not found - cannot build
+    echo "src/main.c not found - cannot build"
     exit 1
 fi
 
 mkdir -p bin
 
 if [ ! -f "bin/sqlite3.o" ]; then
-    echo build sqlite3.o
+    echo "build sqlite3.o"
     gcc -std=c99 -O1 -o bin/sqlite3.o -c src/sqlite3.c -DSQLITE_THREADSAFE=0 -DSQLITE_OMIT_LOAD_EXTENSION
 fi
 
 FLAGS="-std=c99 -Wall -O1"
-case `uname -s` in 
-    *Linux*)
+case `uname` in 
+    Linux*)
         FLAGS="$FLAGS -static"
         ;;
-    *MINGW*)
+    Darwin*)
+        # macOS specific flags
+        ;;
+    MINGW*|MSYS*|CYGWIN*)
         FLAGS="$FLAGS -static"
+        ;;
+    *)
+        echo "Unsupported OS: `uname`"
+        exit 1
         ;;
 esac
 
-echo build sqinn using FLAGS $FLAGS
+echo "build sqinn using FLAGS $FLAGS"
 gcc $FLAGS -o bin/sqinn \
     src/util.c \
     src/mem.c \
@@ -36,6 +43,4 @@ gcc $FLAGS -o bin/sqinn \
     bin/sqlite3.o
 
 mkdir -p dist
-
 cp bin/sqinn dist/
-
